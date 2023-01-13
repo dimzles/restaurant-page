d227af6d8375b76685c1593b698f732e30d541b4
dimzles <dimzlestv@gmail.com>
2023-01-13 20:16:16 +0000

    Add styling for header and buttons

--- src/initialPage.js
+++ src/initialPage.js
@@ -1,9 +1,41 @@
 import chefPlating from './chef-plating.jpg';
 
+const createHeaderLinks = () => {
+    const headerLinks = document.createElement('div');
+    headerLinks.classList.add('header-links');
+
+    const homeLink = document.createElement('button');
+    const aboutLink = document.createElement('button');
+    const menuLink = document.createElement('button');
+    const contactLink = document.createElement('button');
+
+    homeLink.setAttribute('id', 'home')
+    aboutLink.setAttribute('id', 'about')
+    menuLink.setAttribute('id', 'menu')
+    contactLink.setAttribute('id', 'contact');
+
+    homeLink.textContent = 'Home';
+    aboutLink.textContent = 'About';
+    menuLink.textContent = 'Menu';
+    contactLink.textContent = 'Contact';
+
+    headerLinks.appendChild(homeLink);
+    headerLinks.appendChild(aboutLink);
+    headerLinks.appendChild(menuLink);
+    headerLinks.appendChild(contactLink);
+
+    return headerLinks;
+}
+
 const createHeader = () => {
     const header = document.createElement('div')
-    header.textContent = 'Very nice restaurant';
+    const headerText = document.createElement('p');
+
+    headerText.textContent = "Restaurant Name";
+    headerText.classList.add('header-text');
     header.classList.add('header');
+    header.appendChild(headerText);
+    header.appendChild(createHeaderLinks())
 
     return header;
 }
@@ -12,10 +44,13 @@ const createMain = () => {
     const main = document.createElement('div');
     const mainText = document.createElement('p');
     const img = document.createElement('img');
+
     img.src = chefPlating;
     main.classList.add('main');
-    mainText.textContent = 'Very nice food'
-    main.appendChild(img)
+    mainText.classList.add('text');
+    mainText.textContent = 'Very nice food';
+
+    main.appendChild(img);
     main.appendChild(mainText);
 
     return main;
@@ -25,7 +60,7 @@ const createPage = () => {
     const content = document.getElementById('content');
 
     content.appendChild(createHeader());
-    content.appendChild(createMain())
+    content.appendChild(createMain());
 }
 
 export default createPage;
